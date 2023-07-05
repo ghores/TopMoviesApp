838e2ae81f64d64a83f66311c570083fa9ea5084
Ghazimoradi <ghazimoradi.amin@gmail.com>
2023-07-05 21:24:10 +0330

    implementation HomeFragment and fragment_home.xml

--- app/src/main/java/com/example/topmovies/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/topmovies/ui/home/HomeFragment.kt
@@ -6,16 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.topmovies.R
+import com.example.topmovies.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
+    //Binding
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //InitViews
+        binding.apply {
+
+        }
     }
 
 }
