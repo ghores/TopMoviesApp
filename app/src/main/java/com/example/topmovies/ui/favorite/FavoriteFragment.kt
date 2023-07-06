045f6f67d3c59cd78d0d7063be9e1688ea35cfe4
Ghazimoradi <ghazimoradi.amin@gmail.com>
2023-07-06 10:41:44 +0330

    implementation FavoriteFragment

--- app/src/main/java/com/example/topmovies/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/topmovies/ui/favorite/FavoriteFragment.kt
@@ -5,16 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.topmovies.R
+import com.example.topmovies.databinding.FragmentFavoriteBinding
+import com.example.topmovies.utils.initRecycler
+import com.example.topmovies.utils.showInvisible
+import com.example.topmovies.viewmodel.FavoriteViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class FavoriteFragment : Fragment() {
+    //Binding
+    private lateinit var binding: FragmentFavoriteBinding
+
+    @Inject
+    lateinit var favoriteAdapter: FavoriteAdapter
+
+    //Other
+    private val viewmodel: FavoriteViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //InitViews
+        binding.apply {
+            //Show All Favorite
+            viewmodel.loadFavoriteList()
+            //List
+            viewmodel.favoriteList.observe(viewLifecycleOwner) {
+                favoriteAdapter.setData(it)
+                favoriteRecycler.initRecycler(
+                    LinearLayoutManager(requireContext()),
+                    favoriteAdapter
+                )
+            }
+            //Empty
+            viewmodel.empty.observe(viewLifecycleOwner) {
+                if (it) {
+                    emptyItemsLay.showInvisible(true)
+                    favoriteRecycler.showInvisible(false)
+                } else {
+                    emptyItemsLay.showInvisible(false)
+                    favoriteRecycler.showInvisible(true)
+                }
+            }
+        }
     }
 }
